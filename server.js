2754da2354b7b05e3cc560330e69d4d6b3284851
Dave Brown <dfbrownnz@gmail.com>
2025-07-27 15:01:22 -0400

    ad git commands

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+
+  /**
+   * ```
+git init
+
+git add .
+
+git commit -m "Initial commit"
+
+git branch -M main
+
+git remote add origin “Your_Repository”
+
+git push -u origin main
+
+```
+  */
   res.send('Hello, World!');
 });
 
